89f9d4f239c3df759dc7399cb1645fd06b2875a1
Husnain Ali <husnain.dev.009@gmail.com>
2025-11-25 12:01:39 +0500

    Implement sign-in and sign-out & sidebar component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
   	extend: {
   		colors: {
@@ -72,12 +73,14 @@ const config: Config = {
   			}
   		},
   		fontFamily: {
-  			poppins: ["var(--font-poppins)"]
+  			poppins: [
+  				'var(--font-poppins)'
+  			]
   		},
   		boxShadow: {
-  			'drop-1': '0px 10px 30px 0px rgba(66, 71, 97, 0.1)',
-  			'drop-2': '0 8px 30px 0 rgba(65, 89, 214, 0.3)',
-  			'drop-3': '0 8px 30px 0 rgba(65, 89, 214, 0.1)'
+  			'drop-1': '0px 10px 30px rgba(66, 71, 97, 0.1)',
+  			'drop-2': '0px 8px 30px rgba(65, 89, 214, 0.3)',
+  			'drop-3': '0px 8px 30px rgba(65, 89, 214, 0.1)'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
@@ -86,10 +89,10 @@ const config: Config = {
   		},
   		keyframes: {
   			'caret-blink': {
-  				'0%,70%,100%': {
+  				'0%, 70%, 100%': {
   					opacity: '1'
   				},
-  				'20%,50%': {
+  				'20%, 50%': {
   					opacity: '0'
   				}
   			}
@@ -99,6 +102,11 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require("tailwindcss-animate"),
+  ],
 };
+
 export default config;
